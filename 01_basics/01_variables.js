2c1641b6fb9d0fb14623b724f50ef4908a231749
Shivkumar Verma <113050680+KurmiShiv@users.noreply.github.com>
2023-03-20 17:58:56 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,7 @@
+const accountId=14443
+let accountEmail="shiv@gmail.com"
+var accountPassword="12345"
+accountCity="kanpur"
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity])
